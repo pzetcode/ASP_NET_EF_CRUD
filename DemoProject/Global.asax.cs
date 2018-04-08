789e4ad2abb961ba835ecb33e55a3fd0811a60c2
Pawel Zalewski <Pawel.Zalewski@jonckers.com>
2018-04-08 21:56:00 +0200

    Added routes and friendly URLs

--- DemoProject/Global.asax.cs
+++ DemoProject/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
 using System.Web.UI;
+using DemoProject.App_Code;
 
 namespace DemoProject
 {
@@ -17,6 +18,7 @@ namespace DemoProject
                 Path = "~/Scripts/jquery.validate.unobtrusive.min.js",
                 DebugPath = "~/Scripts/jquery.validate.unobtrusive.min.js"
             });
+            RouteConfig.RegisterRoutes(System.Web.Routing.RouteTable.Routes);
         }
     }
 }
