b63abcbcd1cd81cade2dce08ac60208f19789eb0
Pawel Zalewski <Pawel.Zalewski@jonckers.com>
2018-04-09 22:08:42 +0200

    Added basic search page on Factory

---
+++ DemoProject/SearchingPanel.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SearchingPanel.aspx.cs" Inherits="DemoProject.SearchingPanel" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+     <style type="text/css">
+        a{color: beige;          
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:ValidationSummary ShowModelStateErrors="true" runat="server" />    
+    <asp:Label ID="LabelNoRecords" runat="server" Text=""></asp:Label>
+    <asp:GridView runat="server" ID="carsGrid" ItemType="DemoProject.CarsTable" DataKeyNames="Id"
+         SelectMethod="carsGrid_GetData" AutoGenerateColumns="true">         
+    </asp:GridView>
+    <br />
+    <asp:Label ID="LabelFactory" runat="server" Text="Enter factory:"></asp:Label>
+    <asp:TextBox ID="TextBoxFactory" runat="server"></asp:TextBox>
+    <asp:Button ID="ButtonSearch" runat="server" Text="Search" OnClick="ButtonSearch_Click"/>
+    <br />
+
+    
+</asp:Content>
---
+++ DemoProject/SearchingPanel.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DemoProject
+{
+    public partial class SearchingPanel : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LabelNoRecords.Text = "";
+        }      
+
+        protected void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/SearchingPanel?Factory=" + TextBoxFactory.Text);
+        }
+
+        public IQueryable<CarsTable> carsGrid_GetData([QueryString] string factory)
+        {
+            SampleDatabaseEntities db = new SampleDatabaseEntities();
+            var allRows = db.CarsTables.Select(x => x).Where(x => x.Factory == factory);
+
+            //checking if button clicked to avoid message on page load
+            if (allRows.Count()==0 && ClientQueryString.Count() > 0)
+            {
+                LabelNoRecords.Text = "<br />No records found";
+            }
+
+            return allRows;
+        }
+    }
+}
